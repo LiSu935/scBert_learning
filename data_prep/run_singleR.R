7548edc00b9277345f26700e0ef245f9407a195c
LiSu935 <57879262+LiSu935@users.noreply.github.com>
2023-08-31 17:58:29 -0500

    Update run_singleR.R

--- data_prep/run_singleR.R
+++ data_prep/run_singleR.R
@@ -7,6 +7,6 @@ input_dir = "/storage/htc/joshilab/Su_Li/Alg_development/scbert/data/data/"
 
 
 Convert(paste0(input_dir, "TrainData/","seu_obj_few25_s400.h5ad"), dest = "h5seurat", overwrite = TRUE)
-s400_ob <- LoadH5Seurat(paste0(input_dir, "TrainData/","seu_obj_few25_s400.h5seurat"))
+s400_ob <- LoadH5Seurat(paste0(input_dir, "TrainData/","seu_obj_few25_s400.h5seurat")meta.data = FALSE, misc = FALSE)
 
 
