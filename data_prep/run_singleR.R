f4caadcbb168407cae7ae03c461ac898b6e0e495
LiSu935 <57879262+LiSu935@users.noreply.github.com>
2023-09-26 16:07:26 -0500

    Update run_singleR.R

--- data_prep/run_singleR.R
+++ data_prep/run_singleR.R
@@ -4,46 +4,60 @@
 library(Seurat)
 library(SeuratDisk)
 library(SingleR)
+library(optparse)
+
+option_list <- list(
+  make_option(c("-i", "--input_dir"), type = "character", default = "/storage/htc/joshilab/Su_Li/Alg_development/scbert/data/data/TrainData/", 
+              help = "input path", metavar = "character"),
+  make_option(c("-o", "--output_dir"), type = "character", default = '/storage/htc/joshilab/Su_Li/Alg_development/scbert/data/data/TestData/',
+              help = "output path", metavar = "character"),
+  make_option(c("-r","--ref_file"),type = "character",default = "hPancreas_train.h5ad",
+              help = "ref_file", metavar = "character"),
+  make_option(c("-q","--querry_file"), type = "character", default = "hPancreas_test.h5ad",
+              help = "querry_file", metavar = "character")
+)
+
+opt_parser = OptionParser(option_list = option_list)
+opt = parse_args(opt_parser)
+
+
        
-train_input_dir = "/storage/htc/joshilab/Su_Li/Alg_development/scbert/data/data/TrainData/"
-output_dir = '/storage/htc/joshilab/Su_Li/Alg_development/scbert/data/data/TestData/'
+input_dir = opt$input_dir
+output_dir = opt$output_dir
+train_file_input = opt$ref_file
+test_file_input = opt$querry_file
+
 
 train_singler = function(train_file="seu_obj_few25_s400.h5ad", test_file='seu_obj_ForTest.h5ad') {
        train_pre = strsplit(train_file, split='.',fixed=T)[[1]][1]
        test_pre = strsplit(test_file, split='.',fixed=T)[[1]][1]
        
-       Convert(paste0(train_input_dir, train_file), dest = "h5seurat", overwrite = TRUE)
-       s400_ob <- LoadH5Seurat(paste0(train_input_dir, train_pre, ".h5seurat"), meta.data = FALSE, misc = FALSE)
+       Convert(paste0(input_dir, train_file), dest = "h5seurat", overwrite = TRUE)
+       s400_ob <- LoadH5Seurat(paste0(input_dir, train_pre, ".h5seurat"), meta.data = FALSE, misc = FALSE)
        
-       s400_ob_mat = s400_ob@assays$logcounts@data
+       s400_ob_mat = s400_ob@assays$RNA@data
        
-       #Convert(paste0(input_dir, "TestData/","seu_obj_ForTest.h5ad"), dest = "h5seurat", overwrite = TRUE)
-       #test_ob <- LoadH5Seurat(paste0(input_dir, "TestData/","seu_obj_ForTest.h5ad"), meta.data = FALSE, misc = FALSE, commands= FALSE,tools = FALSE )
+       Convert(paste0(input_dir, test_file), dest = "h5seurat", overwrite = TRUE)
+       test_ob <- LoadH5Seurat(paste0(input_dir, test_pre, ".h5seurat"), meta.data = FALSE, misc = FALSE)
+
+       test_mat_m = test_ob@assays$RNA@data
        
        # load test data from csv:
-       test_mat = read.csv(paste0(output_dir, test_pre, "_df.csv"), row.names = 1, header= TRUE)
-       test_mat_m = as.matrix(test_mat)
+       #test_mat = read.csv(paste0(output_dir, test_pre, "_df.csv"), row.names = 1, header= TRUE)
+       #test_mat_m = as.matrix(test_mat)
        
        # load label for the s400_ob_mat:
-       s400_ob_mat_label = read.csv(paste0(train_input_dir,train_pre,".csv"))
+       s400_ob_mat_label = read.csv(paste0(input_dir,train_pre,".csv"))
        
        # run singleR and output the labels
        
-       pred.test <- SingleR(test=test_mat_m, ref=s400_ob_mat, labels=s400_ob_mat_label$cell_type, de.method="wilcox", de.n=50)
+       pred.test <- SingleR(test=test_mat_m, ref=s400_ob_mat, labels=s400_ob_mat_label$celltype, de.method="wilcox", de.n=50)
        table(pred.test$labels)
        write.table(pred.test['labels'], file=paste(output_dir ,test_pre,"_", train_pre, "_singler_labels",".csv",sep=""), row.names = TRUE, col.names = NA, sep=",", quote = FALSE)
        
 }
 
-#train_singler(train_file="seu_obj_few25_s400.h5ad", test_file='seu_obj_ForTest.h5ad')
-
-for (train_file in list.files(path = train_input_dir)) {
-       
-       if (strsplit(train_file, split='.',fixed=T)[[1]][2] == "h5ad") {
-              print(train_file)
-              train_singler(train_file,test_file='seu_obj_ForTest.h5ad')
-       }
-}
+train_singler(train_file=train_file_input,test_file=test_file_input)
 
 
 
@@ -54,3 +68,7 @@ for (train_file in list.files(path = train_input_dir)) {
 #test_df = pd.DataFrame(data=test.layers['logcounts'].toarray(), index=test.obs_names, columns=test.var_names)
 #test_df_t = test_df.T
 #test_df_t.to_csv("/storage/htc/joshilab/Su_Li/Alg_development/scbert/data/data/TestData/test_df.csv")
+
+
+
+
